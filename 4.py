473327ec934bea585ce0eb2a075d5d327f28de81
Gabrijela Grbešić <gabrijelagrbesic5@gmail.com>
2026-03-12 18:55:02 +0100

    update calc

--- 4.py
+++ 4.py
@@ -1,15 +1,21 @@
-a = float(input("Unesi prvi broj: "))
-b = float(input("Unesi drugi broj: "))
+while True:
+    
+    a = float(input("Unesi prvi broj: "))
+    b = float(input("Unesi drugi broj: "))
 
-if b==0:
-    print("Zbroj je ", a+b)
-    print("Razlika je ",a-b)
-    print("Umnozak je ", a*b)
-    print("Nije moguce dijeljenje s nulom!")
-else:    
-    print("Zbroj je ", a+b)
-    print("Razlika je ",a-b)
-    print("Umnozak je ", a*b)
-    print("Kolicnik je ", a/b)
+    if b==0:
+        print("Zbroj je ", a+b)
+        print("Razlika je ",a-b)
+        print("Umnozak je ", a*b)
+        print("Nije moguce dijeljenje s nulom!")
+    else:    
+        print("Zbroj je ", a+b)
+        print("Razlika je ",a-b)
+        print("Umnozak je ", a*b)
+        print("Kolicnik je ", a/b)
+    
+    opet = input("Zelis li opet unijeti?(da/ne)")
+    if opet == "ne":
+        break
 
 
