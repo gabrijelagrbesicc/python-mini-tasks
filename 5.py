85a6e6098765de4c729e4c93fae6f4018b14de67
Gabrijela Grbešić <gabrijelagrbesic5@gmail.com>
2026-03-22 11:50:41 +0100

    using random

---
+++ 5.py
@@ -0,0 +1,30 @@
+"""Napisi program koji ispisuje cjelobrojne elemente koji se pojavljuju 3 ili vise puta unutar
+liste. Generiraj listu od 100 elemenata u rasponu vrijednosti od 0 do 30.
+
+import random
+
+lista = []
+for i in range(100):
+    lista.append(random.randint(0,30))
+
+print("Lista: ",lista)
+
+for broj in range(31):
+    if lista.count(broj) >= 3:
+        print(broj, " se pojavljuje ", lista.count(broj)," puta")"""
+
+""""
+Napiši program koji: generira listu od 50 brojeva (0–20) ispisuje sve brojeve koji se pojavljuju barem 2 puta
+
+import random
+
+lista = []
+
+for i in range(50):
+    lista.append(random.randint(0,20))
+print("Lista: ",lista)
+
+for broj in range(21):
+    if lista.count(broj) >= 2:
+        print(broj, " se ponavlja ",lista.count(broj)," puta")
+"""
