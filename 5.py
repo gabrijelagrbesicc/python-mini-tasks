1f418b9b147280bf158a17511939d431e4c9694a
Gabrijela Grbešić <gabrijelagrbesic5@gmail.com>
2026-03-23 13:49:33 +0100

    counting

--- 5.py
+++ 5.py
@@ -162,7 +162,7 @@ print("Nova lista: ",nova)"""
 
 """
 Napiši program koji: generira listu od 10 brojeva (1–20) ispisuje: originalnu listu listu obrnutim redoslijedom
-"""
+
 
 import random
 
@@ -174,4 +174,67 @@ print("Lista: ",lista)
 
 obrnuta = lista[::-1]
 print("Obrnuta lista: ",obrnuta)
+"""
+
+"""
+Napiši program koji: generira slučajan broj od 1 do 20 korisnik pogađa broj program govori: "veći je" "manji je" dok ne pogodi
+
+import random
+
+broj = random.randint(1,20)
+
+pogodak = int(input("Pogodi broj:"))
+
+while pogodak != broj:
+    if pogodak>broj:
+        print("Broj je manji.Pokusaj opet")
+    else:
+        print("Broj je veci.Pokusaj opet")
+
+    pogodak = int(input("Pogodi broj:"))
+
+print("Bravo pogodili ste broj")"""
+
+"""
+Napiši program koji: generira listu od 50 brojeva (0–15) ispisuje brojeve koji se pojavljuju tačno 1 put
+
+import random
+
+lista = []
+nova = []
+
+for i in range(50):
+    lista.append(random.randint(0,15))
+print("Lista: ",lista)
+
+for broj in range(16):
+    brojac = lista.count(broj)
+
+    if brojac==1:
+        nova.append(broj)
+
+print("Novi brojevi:",nova)"""
+
+"""
+Napiši program koji: generira listu od 100 brojeva (0–20) ispisuje: sve brojeve koji se pojavljuju ≥3 puta koliko takvih brojeva ima
+"""
+
+import random
+
+lista = []
+nova = []
+koliko=0
+for i in range(100):
+    lista.append(random.randint(0,20))
+print("Lista: ",lista)
+
+for broj in range(21):
+    brojac = lista.count(broj)
+
+    if brojac >= 3:
+        nova.append(broj)
+        koliko += 1
+
+print("Brojevi koji se ponavljanju 3 ili vise puta su:",nova)
+print("Takvih brojeva je: ",koliko)
 
